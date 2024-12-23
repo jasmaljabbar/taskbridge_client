2e7c2e860357b0b0d4f1f70828965acdfd768355
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-12-23 12:32:04 +0530

    change back

--- src/Components/user_side/UserProfile.jsx
+++ src/Components/user_side/UserProfile.jsx
@@ -156,7 +156,7 @@ const UserProfile = () => {
           <div className="flex items-center">
             {profile_data.profile_photo ? (
               <img
-                src={imagePreview || profile_data.profile_photo || Unknown}
+                src={profile_data.profile_photo}
                 alt="Profile"
                 className="w-10 h-10 rounded-full"
               />
